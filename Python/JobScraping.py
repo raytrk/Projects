2420e2521ea431ae570b23f43f76be7e2c60f537
Ray <raytrk>
2024-03-19 21:53:50 +0800

    Changed saved location

--- Python/JobScraping.py
+++ Python/JobScraping.py
@@ -5,10 +5,14 @@
 3. Glassdoor
 
 References:
+Virtual environments: https://docs.python.org/3/library/venv.html#how-venvs-work
 Scraping Linkedin: https://www.scrapingdog.com/blog/scrape-linkedin-jobs/
 Docker: https://www.youtube.com/watch?v=jtBVppyfDbE
+Docker: https://www.youtube.com/watch?v=0UG2x2iWerk
 '''
 
 
 import requests
 import beautifulsoup4 as bs4
+
+print("testing")
