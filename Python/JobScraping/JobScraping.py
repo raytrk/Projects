cbbeeee04b0c6495c050dc93bb7e30eb5f123f43
Ray <raytrk>
2024-03-19 23:04:47 +0800

    Created the base file to work on

--- Python/JobScraping/JobScraping.py
+++ Python/JobScraping/JobScraping.py
@@ -6,13 +6,59 @@
 
 References:
 Virtual environments: https://docs.python.org/3/library/venv.html#how-venvs-work
-Scraping Linkedin: https://www.scrapingdog.com/blog/scrape-linkedin-jobs/
+Scraping Linkedin default file from: https://www.scrapingdog.com/blog/scrape-linkedin-jobs/
 Docker: https://www.youtube.com/watch?v=jtBVppyfDbE
 Docker: https://www.youtube.com/watch?v=0UG2x2iWerk
 '''
 
+#todo: Figure out how to add in the link to the linkedin posting in my csv
+#Figure out how to change the search terms for the python file especially the location
+
 
 import requests
-import bs4
+from bs4 import BeautifulSoup
+import math
+import pandas as pd
+l=[]
+o={}
+k=[]
+target_url='https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Python%20%28Programming%20Language%29&location=Las%20Vegas%2C%20Nevada%2C%20United%20States&geoId=100293800&currentJobId=3415227738&start={}'
+for i in range(0,math.ceil(117/25)):
 
-print("testing2")
+    res = requests.get(target_url.format(i))
+    soup=BeautifulSoup(res.text,'html.parser')
+    alljobs_on_this_page=soup.find_all("li")
+    print(len(alljobs_on_this_page))
+    for x in range(0,len(alljobs_on_this_page)):
+        jobid = alljobs_on_this_page[x].find("div",{"class":"base-card"}).get('data-entity-urn').split(":")[3]
+        l.append(jobid)
+
+target_url='https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{}'
+for j in range(0,len(l)):
+
+    resp = requests.get(target_url.format(l[j]))
+    soup=BeautifulSoup(resp.text,'html.parser')
+
+    try:
+        o["company"]=soup.find("div",{"class":"top-card-layout__card"}).find("a").find("img").get('alt')
+    except:
+        o["company"]=None
+
+    try:
+        o["job-title"]=soup.find("div",{"class":"top-card-layout__entity-info"}).find("a").text.strip()
+    except:
+        o["job-title"]=None
+
+    try:
+        o["level"]=soup.find("ul",{"class":"description__job-criteria-list"}).find("li").text.replace("Seniority level","").strip()
+    except:
+        o["level"]=None
+
+
+
+    k.append(o)
+    o={}
+
+df = pd.DataFrame(k)
+df.to_csv('linkedinjobs.csv', index=False, encoding='utf-8')
+#print(k)
