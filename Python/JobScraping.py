e00f1025f435288ced4fc8fa25415b26ac0e85bf
Ray <raytrk>
2024-03-19 20:58:42 +0800

    Creating a repo within wsl: Ubuntu

---
+++ Python/JobScraping.py
@@ -0,0 +1,14 @@
+# This script serves to scrape websites daily from the following websites
+'''
+1. Linkedin
+2. Mycareersfuture
+3. Glassdoor
+
+References:
+Scraping Linkedin: https://www.scrapingdog.com/blog/scrape-linkedin-jobs/
+Docker: https://www.youtube.com/watch?v=jtBVppyfDbE
+'''
+
+
+import requests
+import beautifulsoup4 as bs4
